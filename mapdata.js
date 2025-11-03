83ffc7b7a13cd715bb5dd597b0fc8c7efcfaa980
oana-bgn <oana@ueberseehub.de>
2025-11-03 18:11:26 +0100

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -1,7 +1,7 @@
 var simplemaps_countrymap_mapdata={
   main_settings: {
    //General settings
-    width: "responsive", //'700' or 'responsive'
+    width: "responsive",
     background_color: "#FFFFFF",
     background_transparent: "yes",
     border_color: "#EFF5F3",
